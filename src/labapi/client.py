062f17120f210a49bfd20ede37483f50278abc89
Christoph Li <christoph.li@nih.gov>
2026-03-27 18:03:42 -0400

    Parse XML responses from raw bytes

--- src/labapi/client.py
+++ src/labapi/client.py
@@ -351,9 +351,7 @@ class Client:
         :raises RuntimeError: If the API request fails or the response is not valid XML.
         """
 
-        return fromstring(
-            bytes(self.raw_api_get(api_method_uri, **kwargs).text, encoding="utf-8")
-        )
+        return fromstring(self.raw_api_get(api_method_uri, **kwargs).content)
 
     def api_post(
         self,
@@ -375,11 +373,7 @@ class Client:
         :raises RuntimeError: If the API request fails or the response is not valid XML.
         """
 
-        return fromstring(
-            bytes(
-                self.raw_api_post(api_method_uri, body, **kwargs).text, encoding="utf-8"
-            )
-        )
+        return fromstring(self.raw_api_post(api_method_uri, body, **kwargs).content)
 
     def default_authenticate(self) -> User:
         """
